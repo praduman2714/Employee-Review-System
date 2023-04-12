fb43c25567c68f82377dc1bc4e210c8deb98c916
praduman2714 <pradu14jsr@gmail.com>
2023-04-12 16:09:22 +0530

    Added the home page, and header

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,7 @@
 module.exports.signIn = function(req, res){
     return res.render('sign_in');
 }
+
+module.exports.signUp = function(req, res){
+    return res.render('sign_up');
+}
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 
 router.get('/sign-in' , userController.signIn);
+router.get('/sign-up' , userController.signUp);
 
 module.exports = router;
