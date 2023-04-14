1675b9e52aafefe1bc2915eddd850dc6eb853bf2
praduman2714 <pradu14jsr@gmail.com>
2023-04-14 16:58:18 +0530

    After reviewing the user will disapper

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -8,15 +8,28 @@ module.exports.newReview = async (req, res) => {
     try {
         // first find recieoient //
         let recipient = await User.findById(req.params.id);
-
+        // console.log(recipient + " recipient ");
         if (!recipient) {
             console.log("Recipient is not valid");
             return res.redirect('/');
         }
+
+       
+        for(let i = 0; i<req.user.userToReview.length ; i++){
+            if (req.user.userToReview[i] == recipient.id) {
+                //console.log("Entered in the loop");
+                req.user.userToReview.splice(i, 1);
+                
+                req.user.save();
+            }
+        }
+
         // we just take reviewer and reviewed 
         for (let i = 0; i < recipient.reviewRecivedFrom.length; i++){
             if (req.user) {
                 if (recipient.reviewRecivedFrom[i] == req.user.id) {
+                    req.user.userToReview.pop(i);
+                    // recipient.reviewRecivedFrom.save();
                     const new_review = Review.create({
                        reviewer : recipient.id,
                         reviewed: req.user.id,
