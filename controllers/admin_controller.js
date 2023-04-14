72530b7df6120a32ae601f129f24c1502de48fd6
praduman2714 <pradu14jsr@gmail.com>
2023-04-14 13:00:12 +0530

    Added the functionality of assignin the task

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -23,3 +23,45 @@ module.exports.showEmployeeList = async function(req, res){
         employes : employeList
     });
 }
+
+
+module.exports.setReviewrAndReviewe = async function(req, res){
+    try{
+        if(!req.isAuthenticated()){
+            // flash messages
+            console.log("Please logIn");
+            return res.redirect('/users/sign-in');
+        }else{
+            let employee = await Users.findById(req.user.id);
+    
+            if(employee.isAdmin == false){
+                // flash Messages
+                console.log('User is not admin');
+                return res.redirect('/users/sign-in');
+            }
+        
+            else if(req.body.sender == req.body.reciver){
+                // flash messages
+                console.log("sender === reciver")
+                return res.redirect('back');
+            }
+        
+            else{
+                let sender = await Users.findById(req.body.sender);
+                let reciver = await Users.findById(req.body.reciver);
+                //console.log(sender + " " + reciver);
+                sender.userToReview.push(reciver);
+                sender.save();
+                reciver.reviewRecivedFrom.push(sender);
+                reciver.save();
+                // flash Messages
+                return res.redirect('back');
+            }
+        }
+    
+        
+    }catch(err){
+        console.log("Errror in setting up the user " + err);
+    }
+
+}
--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,6 @@ const adminController = require('../controllers/admin_controller');
 
 router.get('/assignWork', adminController.assignWork);
 router.get('/view-employee', adminController.showEmployeeList);
+router.post('/setReviewes', adminController.setReviewrAndReviewe);
 
 module.exports = router;
